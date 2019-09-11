7ceb376456bd772943ea8201cd08b095a08b9583
felipe <felipealisboa@outlook.com>
2019-09-11 19:28:47 +0100

    add and edit css for match the table requirement; refactor js file

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,5 +1,7 @@
 import React, { Component, useState } from 'react';
 
+
+
 class Users extends Component {
 
   state = {
@@ -19,25 +21,28 @@ class Users extends Component {
   }
 
 
-
-
   render() {
     return (
       <div className="container">
-        <div className="col-xs-12">
+        <div className="col-md">
         <h1>Users</h1>
         {this.state.users.map((user) => (
           <div className="card">
             <div className="card-body">
             <table class="table table-hover">
-                <thead display="block">
+                <thead>
                   <tr>
                     <th scope="col">#Id</th>
                     <th scope="col">Name</th>
                     <th scope="col">User Name</th>
                     <th scope="col">Email</th>
                     <th scope="col">Zip code</th>
-                    <th scope="col">Action</th>
+                    <th scope="col">
+                    <button type="button" class="btn btn-success btn-sm">Order by Name: A-Z</button>
+                    </th>
+                    <th scope="col">
+                    <button type="button" class="btn btn-danger btn-sm">Order by Zip Code: 0-9</button>
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
@@ -47,11 +52,12 @@ class Users extends Component {
                     <td>{user.username}</td>
                     <td>{user.email}</td>
                     <td>{user.address.zipcode}</td>
+
                   </tr>
                 </tbody>
                 </table>
+                </div>
             </div>
-          </div>
         ))}
         </div>
       </div>

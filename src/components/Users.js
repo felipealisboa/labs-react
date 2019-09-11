570ca2ab36347eee9f5c08b3e84821e7f0095f79
felipe <felipealisboa@outlook.com>
2019-09-11 23:44:48 +0100

    add pagination for the table

--- src/components/Users.js
+++ src/components/Users.js
@@ -20,36 +20,6 @@ class Users extends Component {
       .catch(console.log)
   }
 
-        var people,
-            asc1 = 1,
-            asc2 = 1,
-            asc3 = 1;
-        window.onload = function () {
-            people = document.getElementById("people");
-        }
-
-        function sort_table(tbody, col, asc) {
-            var rows = tbody.rows,
-                rlen = rows.length,
-                arr = new Array(),
-                i, j, cells, clen;
-            // fill the array with values from the table
-            for (i = 0; i < rlen; i++) {
-                cells = rows[i].cells;
-                clen = cells.length;
-                arr[i] = new Array();
-                for (j = 0; j < clen; j++) {
-                    arr[i][j] = cells[j].innerHTML;
-                }
-            }
-            // sort the array by the specified column number (col) and order (asc)
-            arr.sort(function (a, b) {
-                return (a[col] == b[col]) ? 0 : ((a[col] > b[col]) ? asc : -1 * asc);
-            });
-            // replace existing rows with new rows created from the sorted array
-            for (i = 0; i < rlen; i++) {
-                rows[i].innerHTML = "<td>" + arr[i].join("</td><td>") + "</td>";
-            }
 
 
   render() {
@@ -60,12 +30,12 @@ class Users extends Component {
 
           <div className="card">
             <div className="card-body">
-            <table class="table table-hover">
+            <table id="selectedColumn" class="table table-hover table-striped table-bordered table-sm" cellspacing="0" width="100%">
                 <thead>
                   <tr>
-                    <th id="idSort" scope="col">#Id</th>
+                    <th class="sorting" scope="col">#Id</th>
                     <th scope="col">Name</th>
-                    <th id="username" scope="col">User Name</th>
+                    <th class="sorting" scope="col" arial-label="Name: activate to sort column descending" arial-sort="ascending" rowspan="1" colspan="1">User Name</th>
                     <th scope="col">Email</th>
                     <th scope="col">Zip code</th>
                     <th scope="col">Open Detail</th>
@@ -79,7 +49,7 @@ class Users extends Component {
                     <td>{user.username}</td>
                     <td>{user.email}</td>
                     <td>{user.address.zipcode}</td>
-                    <td><button type="button" class="btn btn-success btn-sm">Detail</button></td>
+                    <td><button type="button" class="btn btn-success btn-sm" onCLick={user}>Detail</button></td>
                   </tr>
                 ))}
                 </tbody>
@@ -87,7 +57,37 @@ class Users extends Component {
                 </div>
             </div>
         </div>
+        <div class="row">
+          <div class="col-sm-12 col-md-7">
+            <div class="dataTables_paginate paging_simple_numbers" id="example_paginate">
+              <ul class="pagination">
+                <li class="paginate_button page-intem pevious disabled" id="example_previous">
+                  <a tabindex="0" class="page-link" aria-controls="example" href="#" data-dt-idx="0">Previous</a>
+                </li>
+                <li class="paginate_button page-intem active">
+                  <a tabindex="0" class="page-link" aria-controls="example" href="#" data-dt-idx="1">1</a>
+                </li>
+                <li class="paginate_button page-intem">
+                  <a tabindex="0" class="page-link" aria-controls="example" href="#" data-dt-idx="2">2</a>
+                </li>
+                <li class="paginate_button page-intem">
+                  <a tabindex="0" class="page-link" aria-controls="example" href="#" data-dt-idx="3">3</a>
+                </li>
+                <li class="paginate_button page-intem">
+                  <a tabindex="0" class="page-link" aria-controls="example" href="#" data-dt-idx="4">4</a>
+                </li>
+                <li class="paginate_button page-intem">
+                  <a tabindex="0" class="page-link" aria-controls="example" href="#" data-dt-idx="5">5</a>
+                </li>
+                <li class="paginate_button page-intem next" id="example_next">
+                  <a tabindex="0" class="page-link" aria-controls="example" href="#" data-dt-idx="6">Next</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
+
     );
   }
 }

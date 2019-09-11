0a3498e09248373b15e275ed220ed8fdf9704c0b
felipe <felipealisboa@outlook.com>
2019-09-11 22:10:10 +0100

    insert methods to sort table for a specific id:ref

--- src/components/Users.js
+++ src/components/Users.js
@@ -20,45 +20,72 @@ class Users extends Component {
       .catch(console.log)
   }
 
+        var people,
+            asc1 = 1,
+            asc2 = 1,
+            asc3 = 1;
+        window.onload = function () {
+            people = document.getElementById("people");
+        }
+
+        function sort_table(tbody, col, asc) {
+            var rows = tbody.rows,
+                rlen = rows.length,
+                arr = new Array(),
+                i, j, cells, clen;
+            // fill the array with values from the table
+            for (i = 0; i < rlen; i++) {
+                cells = rows[i].cells;
+                clen = cells.length;
+                arr[i] = new Array();
+                for (j = 0; j < clen; j++) {
+                    arr[i][j] = cells[j].innerHTML;
+                }
+            }
+            // sort the array by the specified column number (col) and order (asc)
+            arr.sort(function (a, b) {
+                return (a[col] == b[col]) ? 0 : ((a[col] > b[col]) ? asc : -1 * asc);
+            });
+            // replace existing rows with new rows created from the sorted array
+            for (i = 0; i < rlen; i++) {
+                rows[i].innerHTML = "<td>" + arr[i].join("</td><td>") + "</td>";
+            }
+
 
   render() {
     return (
       <div className="container">
         <div className="col-md">
         <h1>Users</h1>
-        {this.state.users.map((user) => (
+
           <div className="card">
             <div className="card-body">
             <table class="table table-hover">
                 <thead>
                   <tr>
-                    <th scope="col">#Id</th>
+                    <th id="idSort" scope="col">#Id</th>
                     <th scope="col">Name</th>
-                    <th scope="col">User Name</th>
+                    <th id="username" scope="col">User Name</th>
                     <th scope="col">Email</th>
                     <th scope="col">Zip code</th>
-                    <th scope="col">
-                    <button type="button" class="btn btn-success btn-sm">Order by Name: A-Z</button>
-                    </th>
-                    <th scope="col">
-                    <button type="button" class="btn btn-danger btn-sm">Order by Zip Code: 0-9</button>
-                    </th>
-                  </tr>
+                    <th scope="col">Open Detail</th>
+                </tr>
                 </thead>
                 <tbody>
+                {this.state.users.map((user) => (
                   <tr>
                     <td>{user.id}</td>
                     <td>{user.name}</td>
                     <td>{user.username}</td>
                     <td>{user.email}</td>
                     <td>{user.address.zipcode}</td>
-
+                    <td><button type="button" class="btn btn-success btn-sm">Detail</button></td>
                   </tr>
+                ))}
                 </tbody>
                 </table>
                 </div>
             </div>
-        ))}
         </div>
       </div>
     );
